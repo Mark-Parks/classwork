5b48c4d4d3471b9623820206819e5cd676f03946
Mark Parks <31749006+Mark-Parks@users.noreply.github.com>
2017-09-13 15:36:36 -0400

    Update IntroMain.java

--- MarkParks_/src/intro/IntroMain.java
+++ MarkParks_/src/intro/IntroMain.java
@@ -26,25 +26,3 @@ public class IntroMain {
 		}
 	}
 }
-package intro;
-
-public class CodingConventions {
-
-	//FIELDS ARE AT THE TOP!!!
-	private String name;//NOTE: DECALRE FIELDS ONLY, INSTANTIATE IN THE CONSTRUCTOR
-	private String description;
-	
-	//this is a constructor, it is like a method except the "return type" is an instance of the class, for this reason a constructor must ALWAYS be named after the class
-	public CodingConventions(int nameIndex, int descriptionIndex){
-		//there are two variables called "name" the local and the field, distinguish by using the reserved word "this"(feild)
-		name = IntroMain.NAMES[nameIndex];
-		//this is a static call to a constant
-		description = IntroMain.DESCRIPTIONS[descriptionIndex];
-	}
-	
-	//normal method "void"
-	public void doStuff() {
-		String output = name + description;//use spaces in between operations
-		System.out.println(output);
-	}
-}

e11d8addd79f2dcbc71b8de973fbabb0c523cbe0
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-09-27 14:39:26 -0400

    finished

--- MarkParks_/src/attendance/Student.java
+++ MarkParks_/src/attendance/Student.java
@@ -9,6 +9,7 @@ public class Student implements Attendee{
 	public Student(String firstName, String lastName) {
 		this.fName = firstName;
 		this.lName = lastName;
+		present = false;
 	}
 	
 	public boolean isPresent() {
@@ -16,6 +17,35 @@ public class Student implements Attendee{
 	}
 	
 	public void setPresent(boolean present) {
-		
+		this.present = present;
+	}
+	
+	public String getFirstName() {
+		return this.fName;
+	}
+	
+	public String getLastName() {
+		return this.lName;
+	}
+	
+	public boolean mathces(String first, String last) {
+		return first.toLowerCase().equals(fName.toLowerCase()) && last.toLowerCase().equals(lName.toLowerCase());
+	}
+	
+	public boolean matches(String last) {
+		return last.toLowerCase().equals(lName.toLowerCase());
+	}
+	
+	public String getReportString() {
+		String report = lName;
+		report += "                    ";
+		report = report.substring(0, 20);
+		report += fName;
+		report += "                    ";
+		report = report.substring(0,40);
+		if( this.isPresent()) {
+			return report += "PRESENT";
+		}
+		return report += "ABSENT";
 	}
 }

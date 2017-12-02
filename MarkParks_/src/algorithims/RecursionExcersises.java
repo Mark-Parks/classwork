b8e79c3c2e788bffd77b607c45be4ac31d04f959
Mark Alessandro <Mark Alessandro@MarkAlessandro.fios-router.home>
2017-12-01 19:48:12 -0500

    Hanoi Towers Done

--- MarkParks_/src/algorithims/RecursionExcersises.java
+++ MarkParks_/src/algorithims/RecursionExcersises.java
@@ -3,23 +3,33 @@ package algorithims;
 public class RecursionExcersises {
 	
 	 public static void main(String[] args){
-		 solve(3,"A","B","C");
+		 solve(4,"A","B","C");
 	 }
 
 	 private static void solve(int n, String start, String help, String end) {
 		if(n==1) {
-			System.out.println("_______");
-		}
-		else { 
-			if(n%2 == 0) {
-				System.out.println(start+" to "+help);
-				solve(n-1, help, end, start);
-			}else {
-				System.out.println(start+" to "+end);
-				solve(n-1, start, help, end);
-			}
+			/**THIS IS THE BASE CASE NO MATTER WHAT WHEN A TOWER'S HEIGHT IS 1
+			 * IT MOVES TO THE PILLAR THAT IS NOT DEFINE AS THE PREVIOUS HELP
+			 */
+			System.out.println(start+" to "+end);
+		}else {
+			/**SINCE WE JUST REMOVED A DISC WE GOTTA n-1 BUT ALSO
+			 * REARRANGED THE PAREMETERS BECAUSE WE ARN'T ONLY MOVING TO THE END PILLAR
+			 */
+			solve(n-1, start, end, help);
+			/**NOW THAT THE PARAMETERS WERE CHANGED WE SAY THIS AGAIN
+			 * BECUASE THE HELP IS NOW OUR END BUT STILL THE SAME PILLAR
+			 */
+			System.out.println(start+" to "+end);
+			/**AND NOW THAT WE ARE ON THE HELP PILLAR
+			 * WHICH WAS OUR PREVIOUS END PILLAR 
+			 * WE MOVE IT BACK TO THE PRIVOUS END WHICH WAS THE HELP
+			 * AND USE THE STARTING PILLAR AS OUR NEW HELP
+			 */
+			solve(n-1, help, start, end);
 		}
 	}
+	
 
 	public static long countPrimeFactors(long value, long testPrime) {
 		 if(value/testPrime == 1) {
